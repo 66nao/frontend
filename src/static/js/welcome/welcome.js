f55f2ead5eb03bf759f16de5ba236dd43d9cb473
erguotou <erguotou525@gmail.com>
2015-12-04 17:39:52 +0800

    change to vue router

---
+++ src/static/js/welcome/welcome.js
@@ -0,0 +1,10 @@
+/**
+ * Created by erguotou on 2015/12/2.
+ */
+
+'use strict';
+define(function (require, exports, module) {
+  module.exports = {
+    template: require('../../../view/welcome/welcome.html', 'html')
+  };
+});
