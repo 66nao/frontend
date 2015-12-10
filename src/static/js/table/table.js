ec6ca1ceae7ff6cb1ffbb0ab6057ffefc0b36a31
erguotou <erguotou525@gmail.com>
2015-12-10 11:32:27 +0800

    路由实例

--- src/static/js/table/table.js
+++ src/static/js/table/table.js
@@ -4,7 +4,49 @@
 
 'use strict';
 define(function (require, exports, module) {
+  Vue.filter('evaluation', function (value) {
+    var tmp = Math.ceil(parseInt(value) / 10);
+    if (tmp >= 9) {
+      return '优秀';
+    } else if (tmp >= 6) {
+      return '良好';
+    } else {
+      return '不及格';
+    }
+  });
   module.exports = {
-    template: require('../../../view/table/table.html', 'html')
+    template: require('../../../view/table/table.html', 'html'),
+    data: function() {
+      return {
+        search: '',
+        orderBy: '',
+        orderReverse: 1,
+        students: null
+      };
+    },
+    methods : {
+      fOrder: function (name, reverse) {
+        this.orderBy = name;
+        this.orderReverse = reverse;
+      }
+    },
+    route: {
+      activate: function (transition) {
+        this.students = [{
+          name: 'Li Lei',
+          score: 75
+        }, {
+          name: 'Han Meimei',
+          score: 82
+        }, {
+          name: 'Jerry',
+          score: 59
+        }, {
+          name: 'Tom',
+          score: 90
+        }];
+        transition.next();
+      }
+    }
   };
 });
