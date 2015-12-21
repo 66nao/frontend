01ab6355c32855355f4c75dc78232948924a48e8
huying <huying@66nao.com>
2015-12-21 16:04:03 +0800

    merge

--- src/app/sub-menu/sub/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by huying on 2015/12/18.
- */
-'use strict';
-define(function (require, exports, module) {
-    module.exports = {
-      template: require('./main.html', 'html')
-    };
-});
