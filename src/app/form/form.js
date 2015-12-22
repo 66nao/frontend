974a688677725ebd10a1b0e1d5842e499c7f3d41
huayuehong <>
2015-12-22 16:24:45 +0800

    form表单验证添加

--- src/app/form/form.js
+++ src/app/form/form.js
@@ -3,24 +3,32 @@
  */
 'use strict';
 define(function (require, exports, module) {
-
+  Vue.validator('call', function (val) {
+    return /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(val)
+  })
   module.exports = {
     template: require('./form.html','html'),
     data: function() {
       return {
         isSubmitted: false,
-        comment: ''
+        comment: '',
+        test:null,
+        hand:null
       };
-    },
-    methods: {
-      doSubmit: function() {
-        this.isSubmitted = true;
-      }
-    },
-    watch: {
-      comment: function() {
-        this.isSubmitted = false;
-      }
     }
+    //methods: {
+    //  doSubmit: function() {
+    //    this.isSubmitted = true;
+    //  }
+    //},
+    //watch: {
+    //  comment: function() {
+    //    this.isSubmitted = false;
+    //  }
+    //}
+
+
+
+
   };
 });
